a64d803e0ac7fa1f983c8fd49ea2309ff1f1e0c6
Yaojie Li <jeffrey.yaojie.li@gmail.com>
2023-03-05 00:40:55 -0500

    Test Server

--- Assets/_Scripts/Networking/NetworkManager.cs
+++ Assets/_Scripts/Networking/NetworkManager.cs
@@ -11,25 +11,37 @@ using System.Threading.Tasks;
 
 public class NetworkManager : MonoBehaviour
 {
-    private static byte[] data = new byte[1024];
+    
     private static IPEndPoint remoteEP;
     private static Socket clientSocket;
 
-    public static int playerID;
+    public static short playerID;
 
 
 
     // Start is called before the first frame update
     void Start()
     {
-        IPAddress ip = IPAddress.Parse("192.168.2.43");
-        remoteEP = new IPEndPoint(ip, 12581);
+        IPAddress ip = IPAddress.Parse("127.0.0.1");
+        remoteEP = new IPEndPoint(ip, 8888);
         clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
 
         clientSocket.Connect(remoteEP);
 
-        data = Encoding.ASCII.GetBytes("TestPlayerName");
-        clientSocket.Send(data);
+        
+
+        short[] header = { 0 };
+        byte[] playerName = Encoding.ASCII.GetBytes("TestPlayerName");
+
+        byte[] initMsg = new byte[header.Length * 2 + playerName.Length];
+
+        Buffer.BlockCopy(header, 0, initMsg, 0, header.Length * 2);
+
+        Buffer.BlockCopy(playerName, 0, initMsg, header.Length * 2, playerName.Length);
+
+        clientSocket.Send(initMsg);
+
+        Debug.Log("FIRST SEND");
 
         Task.Run(() => { clientReceive(); });
     }
@@ -42,12 +54,37 @@ public class NetworkManager : MonoBehaviour
 
     static void clientReceive()
     {
-        byte[] buffer = new byte[1024];
-        int recv = clientSocket.Receive(buffer);
+        Debug.Log("Start Receive");
+        try
+        {
+            byte[] buffer = new byte[1024];
+            int recv = clientSocket.Receive(buffer);
 
-        string str = Encoding.ASCII.GetString(buffer);
+            short[] shortBuffer = new short[1];
+            Buffer.BlockCopy(buffer, 0, shortBuffer, 0, 2);
+            byte[] byContent = new byte[recv - 2];
+            Buffer.BlockCopy(buffer, 2, byContent, 0, byContent.Length);
+            Debug.Log("Received!");
 
-        Debug.Log("GETT! : " + str);
-        playerID = int.Parse(str);
+            switch (shortBuffer[0])
+            {
+                // First Login
+                case 0:
+                    Buffer.BlockCopy(byContent, 0, shortBuffer, 0, byContent.Length);
+                    playerID = shortBuffer[0];
+                    Debug.Log("PlayerID Set TO:" + playerID);
+                    break;
+
+                default:
+                    Debug.Log("Default");
+                    break;
+            }
+        }
+        catch (Exception ex)
+        {
+            Debug.Log(ex.ToString());
+        }
+        
+
     }
 }
