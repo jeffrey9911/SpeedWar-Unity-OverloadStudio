b98e66e825d25d5fb5fc02ef527664d6b3f1b401
Jeffrey Li <jeffrey.yaojie.li@gmail.com>
2023-04-05 16:43:30 -0400

    Cinematic Scene + SFX

---
+++ Assets/GarageCinematic/cube.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cube : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Input.GetAxis("Horizontal") * Time.deltaTime * 2f, 
+            0, Input.GetAxis("Vertical") * Time.deltaTime *2f);   
+    }
+}
